d032bc2a3be76a1b6fc74fa597400cefe193265e
Mohan51502 <rajmohan51502@gmail.com>
2023-07-09 10:27:52 +0530

    commit

--- src/components/MainDash/MainDash.jsx
+++ src/components/MainDash/MainDash.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Cards from "../Cards/Cards";
 import Table from "../Table/Table";
 import "./MainDash.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 
 
